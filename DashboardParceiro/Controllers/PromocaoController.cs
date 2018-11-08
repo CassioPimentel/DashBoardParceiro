fafcd9eaa391204ed7a0ba64717b3aa10439588c
ODILONSANTOS\Cassio <cassiopimentel@odilonsantos.com>
2018-11-08 17:33:47 -0200

    Class promoção

---
+++ DashboardParceiro/Controllers/PromocaoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DashboardParceiro.Controllers
+{
+    public class PromocaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
