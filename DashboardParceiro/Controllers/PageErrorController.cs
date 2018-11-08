217f4feaa320dde5200d4dd84f055b3b1214a623
ODILONSANTOS\Cassio <cassiopimentel@odilonsantos.com>
2018-11-08 14:49:41 -0200

    page error

---
+++ DashboardParceiro/Controllers/PageErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DashboardParceiro.Controllers
+{
+    public class PageErrorController : Controller
+    {
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
